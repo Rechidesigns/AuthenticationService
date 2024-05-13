2932feca2719b42fe58a28bb242e3264445850d6
Alerechi Ordu <alerechiordu@gmail.com>
2024-05-13 17:25:42 +0100

    fixed bugs

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -16,8 +16,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
